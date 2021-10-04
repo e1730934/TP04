d5215a1230a7cab4d99ee0e28ef721fd0dc781a4
e1730934 <e1730934@cmaisonneuve.qc.ca>
2021-10-03 21:12:31 -0400

    Modification d'étudiant.js et de main.js afin de refléter le commentaire de @sfostine sur les routes modulaire

--- etudiant.js
+++ etudiant.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const app = express()
-const departement = require("./données_dep");
-const port = 3000
-app.use(express.urlencoded({extended: true}));
+const express = require('express');
+const donneDepartement = require("./données_dep");
+const router = express.Router();
 
 
-app.get('/etudiants', (req, res) => {
-    res.send(listeEtudiant())
-})
-app.route(['/:identifiantCours/etudiants'])
-    .get((req, res) => {
+
+router.get('/etudiants', function (req, res) {
+    res.send(listeEtudiant());
+});
+
+router.route(['/:identifiantCours/etudiants'])
+    .get(function (req, res) {
         let cours = req.params.identifiantCours;
-        res.send(listeEtudiantParCours(cours))
+        res.send(listeEtudiantParCours(cours));
     })
 
     .post(function (req, res) {
-        let cours = departement.cours
+        let cours = donneDepartement.cours;
         for (let i = 0; i < cours.length; i++) {
             if (cours[i].identifiant === '420-3D5') {
                 let nouveauEtudiant =
@@ -24,115 +24,108 @@ app.route(['/:identifiantCours/etudiants'])
                         "nom": req.body.nom,
                         "prenom": req.body.prenom,
                         "note": req.body.note
-                    }
+                    };
                 for (let j = 0; j < cours[i].etudiants.length; j++) {
                     if (nouveauEtudiant.numero === (cours[i].etudiants[j]).numero) {
-                        res.status(409).send("L'étudiant fait déja partie du cours")
+                        res.status(409).send("L'étudiant fait déja partie du cours");
                     }
 
                 }
-                cours[i].etudiants.push(nouveauEtudiant)
-                res.send(nouveauEtudiant)
+                cours[i].etudiants.push(nouveauEtudiant);
+                res.send(nouveauEtudiant);
 
             }
         }
         res.send('Requête POST reçu');
     });
 
-app.route(['/:identifiantCours/:numeroEtudiant', '/:identifiantCours/etudiant/:numeroEtudiant'])
+router.route(['/:identifiantCours/:numeroEtudiant', '/:identifiantCours/etudiant/:numeroEtudiant'])
 
-    .get((req, res) => {
+    .get(function (req, res){
         let cours = req.params.identifiantCours;
         let numEtudiant = req.params.numeroEtudiant;
-        let info = infoEtudiant(cours, numEtudiant)
+        let info = infoEtudiant(cours, numEtudiant);
         if (info !== null)
-            res.send(info)
+            res.send(info);
         else
-            res.status(404).send("L'étudiant n'existe pas dans le cours donnée.")
+            res.status(404).send("L'étudiant n'existe pas dans le cours donnée.");
     })
-    .delete((req, res) => {
+    .delete(function (req, res) {
         let cours = req.params.identifiantCours;
         let numEtudiant = req.params.numeroEtudiant;
-        let info = infoEtudiant(cours, numEtudiant)
+        let info = infoEtudiant(cours, numEtudiant);
         if (info === null)
-            res.status(404).send("L'étudiant n'existe pas dans le cours donnée.")
+            res.status(404).send("L'étudiant n'existe pas dans le cours donnée.");
         else {
-            let suppressionEtudiant = deleteEtudiant(cours, numEtudiant)
-            res.send(`Cours: ${departement.cours[suppressionEtudiant[1]].identifiant}, ${departement.cours[suppressionEtudiant[1]].titre} enseigné par ${departement.cours[suppressionEtudiant[1]].professeur}.\n${suppressionEtudiant[0]} `)
+            let suppressionEtudiant = deleteEtudiant(cours, numEtudiant);
+            res.send(`Cours: ${donneDepartement.cours[suppressionEtudiant[1]].identifiant}, ${donneDepartement.cours[suppressionEtudiant[1]].titre} enseigné par ${donneDepartement.cours[suppressionEtudiant[1]].professeur}.\n${suppressionEtudiant[0]} `);
             // res.send(suppressionEtudiant[0] + departement.cours[suppressionEtudiant[1]])
             // res.send( departement.cours[suppressionEtudiant[1]].)
         }
 
-    })
-
-
-app.listen(port, () => {
-    console.log(`La liste de tout les étudiants peuvent être retrouvé sur http://localhost:${port}/etudiants`)
-    console.log(`La liste de tout les étudiants du cours 420-3D5 peuvent être retrouvé sur http://localhost:${port}/420-3D5/etudiants`)
-    console.log(`Les informations de l'étudiant provenant d'un cours peuvent être retrouvé sur http://localhost:${port}/:identifiantCours/:numeroEtudiant (exemple: http://localhost:3000/420-3D5/etudiant/234234234)`)
-})
+    });
 
 function listeEtudiant() {
-    let lesEtudiants = []
-    let cours = departement.cours
+    let lesEtudiants = [];
+    let cours = donneDepartement.cours;
     for (let i = 0; i < cours.length; i++) {
-        let coursSelectionner = cours[i]
-        let listeEtudiantCours = coursSelectionner.etudiants
+        let coursSelectionner = cours[i];
+        let listeEtudiantCours = coursSelectionner.etudiants;
         for (let j = 0; j < listeEtudiantCours.length; j++) {
-            let etudiantSelectionner = Object.assign({}, listeEtudiantCours[j])
-            delete etudiantSelectionner.note
+            let etudiantSelectionner = Object.assign({}, listeEtudiantCours[j]);
+            delete etudiantSelectionner.note;
             if (lesEtudiants.length === 0) {
-                lesEtudiants.push(etudiantSelectionner)
+                lesEtudiants.push(etudiantSelectionner);
             } else {
                 for (let k = 0; k < lesEtudiants.length; k++) {
                     if (etudiantSelectionner.numero !== (lesEtudiants[k])[0]) {
-                        lesEtudiants.push(etudiantSelectionner)
-                        break
+                        lesEtudiants.push(etudiantSelectionner);
+                        break;
                     }
                 }
             }
         }
     }
-    return lesEtudiants
+    return lesEtudiants;
 }
 
 function listeEtudiantParCours(coursSelectionner) {
-    let lesEtudiants = []
-    let cours = departement.cours
+    let lesEtudiants = [];
+    let cours = donneDepartement.cours;
     for (let i = 0; i < cours.length; i++) {
         if (cours[i].identifiant === coursSelectionner) {
             for (let j = 0; j < (cours[i].etudiants).length; j++) {
-                lesEtudiants.push((cours[i]).etudiants[j])
+                lesEtudiants.push((cours[i]).etudiants[j]);
             }
         }
     }
-    return lesEtudiants
+    return lesEtudiants;
 }
 
 function infoEtudiant(cours, numEtudiant) {
-    for (let i = 0; i < departement.cours.length; i++) {
-        if (departement.cours[i].identifiant === cours) {
-            for (let j = 0; j < departement.cours[i].etudiants.length; j++) {
-                let etudiantSelectionner = departement.cours[i].etudiants[j]
+    for (let i = 0; i < donneDepartement.cours.length; i++) {
+        if (donneDepartement.cours[i].identifiant === cours) {
+            for (let j = 0; j < donneDepartement.cours[i].etudiants.length; j++) {
+                let etudiantSelectionner = donneDepartement.cours[i].etudiants[j];
                 if (etudiantSelectionner.numero === numEtudiant) {
-                    return (departement.cours[i].etudiants[j])
+                    return (donneDepartement.cours[i].etudiants[j]);
                 }
             }
         }
 
     }
-    return null
+    return null;
 }
 
 function deleteEtudiant(cours, numEtudiant) {
-    for (let i = 0; i < departement.cours.length; i++) {
-        if (departement.cours[i].identifiant === cours) {
-            for (let j = 0; j < departement.cours[i].etudiants.length; j++) {
-                let etudiantSelectionner = departement.cours[i].etudiants[j]
+    for (let i = 0; i < donneDepartement.cours.length; i++) {
+        if (donneDepartement.cours[i].identifiant === cours) {
+            for (let j = 0; j < donneDepartement.cours[i].etudiants.length; j++) {
+                let etudiantSelectionner = donneDepartement.cours[i].etudiants[j];
                 if (etudiantSelectionner.numero === numEtudiant) {
-                    let etudiantSupprimer = departement.cours[i].etudiants[j]
-                    delete (departement.cours[i].etudiants[j])
-                    return ([`L'étudiant ${etudiantSupprimer.numero} - ${etudiantSupprimer.nom}, ${etudiantSupprimer.prenom} à été supprimé.`, i])
+                    let etudiantSupprimer = donneDepartement.cours[i].etudiants[j];
+                    delete (donneDepartement.cours[i].etudiants[j]);
+                    return ([`L'étudiant ${etudiantSupprimer.numero} - ${etudiantSupprimer.nom}, ${etudiantSupprimer.prenom} à été supprimé.`, i]);
 
                 }
             }
@@ -140,3 +133,5 @@ function deleteEtudiant(cours, numEtudiant) {
 
     }
 }
+
+module.exports=router;
